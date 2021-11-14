306ca1252cf5543b36498fb0d2e87cb0ee60d707
Jake Tronge <jtronge3@gmail.com>
2021-11-14 09:08:33 -0500

    Add initial files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='AdvDB NN',
+    version='0.1',
+    # TODO: Add scripts to run here
+    scripts=[],
+    # Find all packages in the source directory
+    packages=find_packages('src'),
+)
